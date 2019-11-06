5185cbc9cb7bc0a430f71745afcb3d5142c6e9af
mrzahidali <2018cs136@student.uet.edu.pk>
2019-11-06 23:52:20 +0500

    file uploaded

---
+++ creat-BST.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+class node{
+	public:
+		int data;
+		node *left, *right;
+};
+class linkList{
+	private:
+		node *root;
+	public:
+		linkList(){
+			root = NULL;
+		}
+		void creatList(int value){
+			node *temp = new node;
+			node *copyRoot = root;
+			temp -> data = value;
+			temp -> left = NULL;
+			temp -> right = NULL;
+			if(root == NULL){
+				root = temp;
+				copyRoot = temp;
+			}
+			else{
+			while(copyRoot != NULL ){
+				if(temp -> data < copyRoot -> data){
+					if(copyRoot -> left == NULL){
+						copyRoot -> left = temp;
+						return;
+					}else{
+						copyRoot = copyRoot -> left;
+					}
+				}else{
+					if(copyRoot -> right == NULL){
+						copyRoot -> right = temp;
+						return;
+					}else{
+						copyRoot = copyRoot -> right;
+					}
+				}
+			}
+			}
+			
+		}
+		void printInorder(){
+			node *copyRoot = root;
+			cout << copyRoot -> data << copyRoot -> left -> data << copyRoot -> right -> data;
+			
+		}
+};
+int main(){
+	linkList B;
+	B.creatList(3);
+	B.creatList(4);
+	B.creatList(2);
+	B.creatList(30);
+	B.creatList(1);
+	B.creatList(12);
+	B.printInorder();
+	return 0;
+}
